db5ee6c404274fc4384140216dcdcfbc3577482d
Digos <diogobissolimoreno@gmail.com>
2026-03-15 18:36:53 -0300

    refactor: configurando puglin para Vue tanstack query

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,9 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import vScrollEnd from './directives/vScrollEnd'
+import { VueQueryPlugin } from '@tanstack/vue-query'
 
 createApp(App)
   .directive('scroll-end', vScrollEnd)
+  .use(VueQueryPlugin)
   .mount('#app')
