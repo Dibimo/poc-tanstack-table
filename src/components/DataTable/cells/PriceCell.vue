4df3d7acc7b497b23f4bb282961acba9e082b23f
Digos <diogobissolimoreno@gmail.com>
2026-03-15 18:50:13 -0300

    feat: célula para exibir preço

---
+++ src/components/DataTable/cells/PriceCell.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup>
+import { computed } from 'vue'
+import { formatBRL } from '../../../utils/currency'
+
+const { value } = defineProps<{ value: number }>()
+
+const formatted = computed(() => formatBRL(value))
+</script>
+
+<template>
+  <span class="font-medium text-gray-800 dark:text-gray-200">{{ formatted }}</span>
+</template>
